263f9c08d549be7049f9e93c6ade200ec40ae72c
Aarnoud Meijer <aarnoudmeijer@gmail.com>
2020-07-08 14:01:05 +0200

    blog controller update

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -9,7 +9,21 @@ class BlogController extends Controller
 {
     public function index(){
 //        Message::all();
+/*
+$table->id();
+$table->string('title');
+$table->text('body');
+$table->string('autor');
+$table->timestamps();
+*/
+    }
 
+    public function createBlog(Request $request){
+        $blog = new Blog();
+        $blog->title = $request->title;
+        $blog->body = $request->body;
+        $blog->autor = $request->autor;
 
+        $blog->save();
     }
 }
